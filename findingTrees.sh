3ac73642990b9d7a894addba1ccac86e1d7807fa
Aby Bigasin <tetragonal@Abys-MBP.home>
2021-07-28 15:30:48 -0700

    added more paths

--- findingTrees.sh
+++ findingTrees.sh
@@ -4,10 +4,13 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH -t 48:00:00
-grep -o 'path' config.txt | cut -f2- -d:
+OUTPUT=$(dirname $(grep -w 'pathtree' config.txt | cut -f2 -d:))
+SEQOUTPUT=$(dirname $(grep -w 'pathseq' config.txt | cut -f2 -d:))
+
 while IFS= read -r line || [[ -n $line ]]; do
     echo "Gene: $line"
     mkdir $line
-    grep -l $line /Volumes/LaCie/Documents/lyonslab/orthofinder/select_genetrees/* > $line/trees.txt
-    grep -l $line /Volumes/LaCie/Documents/lyonslab/orthofinder/select_genetrees/* | xargs cp -t $line
+    grep -l $line ${OUTPUT}/* > $line/trees.txt
+    grep -l $line ${OUTPUT}/* | xargs -I '{}' cp '{}' $line/
+    grep -l $line ${SEQOUTPUT}/* | xargs -I '{}' cp '{}' $line/
 done < gene_names.txt
