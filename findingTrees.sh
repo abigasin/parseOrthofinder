4ffa8b54e618e36a32e80d923450ce3a7fdeb827
Aby Bigasin <tetragonal@Abys-MBP.home>
2021-08-12 06:55:59 -0700

    Updated finding trees

--- findingTrees.sh
+++ findingTrees.sh
@@ -2,15 +2,27 @@
 #SBATCH --job-name=parseOrthofinder
 #SBATCH --partition=shared
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH -t 48:00:00
+#SBATCH --account=sio138
+#SBATCH --ntasks-per-node=16
+#SBATCH -t 2:00:00
+#SBATCH --mem=25GB
+#SBATCH --output=output-%x.%j.out
 OUTPUT=$(dirname $(grep -w 'pathtree' config.txt | cut -f2 -d:))
-SEQOUTPUT=$(dirname $(grep -w 'pathseq' config.txt | cut -f2 -d:))
+day=$(date +'%B_%d')
+mkdir Results_${day}
+cd Results_${day}/
 
 while IFS= read -r line || [[ -n $line ]]; do
     echo "Gene: $line"
     mkdir $line
-    grep -l $line ${OUTPUT}/* > $line/trees.txt
-    grep -l $line ${OUTPUT}/* | xargs -I '{}' cp '{}' $line/
-    grep -l $line ${SEQOUTPUT}/* | xargs -I '{}' cp '{}' $line/
-done < gene_names.txt
+    ORTHOGROUP=$(grep -w $line ${OUTPUT}/Orthogroups/Orthogroups.tsv | awk '{print $1}')
+    grep -w $line ${OUTPUT}/Orthogroups/Orthogroups.tsv | awk '{print $1}' > $line/orthologue.txt
+    head -1 ${OUTPUT}/Orthogroups/Orthogroups.tsv > $line/orthogroup.tsv
+    grep -w $line ${OUTPUT}/Orthogroups/Orthogroups.tsv >> $line/orthogroup.tsv
+    grep -w ${ORTHOGROUP} ${OUTPUT}/Orthogroups/Orthogroups.GeneCount.tsv >> $line/orthogroup.tsv
+    mkdir $line/Resolved_Gene_Trees
+    cp ${OUTPUT}/Resolved_Gene_Trees/${ORTHOGROUP}_tree.txt $line/Resolved_Gene_Trees/
+    mkdir $line/Orthologues
+    cp ${OUTPUT}/Orthogroup_Sequences/${ORTHOGROUP}.fa $line/Orthologues/
+    
+done < ../gene_names.txt
